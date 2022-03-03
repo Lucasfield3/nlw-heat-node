5f9cfebb9022a92c02062c36e0bbc49bd583b276
Lucasfield3 <lucasclic@yahho.com.br>
2022-03-02 22:20:37 -0300

    prisma client error

--- src/prisma/index.ts
+++ src/prisma/index.ts
@@ -1,5 +1,7 @@
-const PrismaClient =  require("prisma/prisma-client");
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const { PrismaClient } = require("@prisma/client");
 
-const prismaClient = new PrismaClient()
+const prismaClient = new PrismaClient();
 
 export default prismaClient;
