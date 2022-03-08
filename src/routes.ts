a0eccc8ab5c072616019cd133a86011b320927df
Lucasfield3 <lucasclic@yahho.com.br>
2022-03-07 22:17:47 -0300

    ensure

--- src/routes.ts
+++ src/routes.ts
@@ -9,9 +9,9 @@ import ensureAuthenticated from './middleware/ensureAuthenticated'
 const router = Router()
 
 router.post('/authenticate', new AuthenticateUserController().handle)
-router.post('/messages', new CreateMessageController().handle)
+router.post('/messages', ensureAuthenticated, new CreateMessageController().handle)
 
 router.get('/messages/last-3', new Get3LastMessagesController().handle)
-router.get('/profile', new ProfileUserController().handle)
+router.get('/profile', ensureAuthenticated, new ProfileUserController().handle)
 
 export { router }
